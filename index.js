a204d56a199a431b4f17922727db271980934de7
Patrick Coffey <patrickcoffey48@gmail.com>
2016-12-02 14:04:50 -0600

    Adhere to linting rules

--- index.js
+++ index.js
@@ -13,34 +13,34 @@ const client = new Twitter(require('./config.js'));
  * @param {number} count Number of tweets that should be fetched.
  * @returns {object} Promise object that resolves an array of tweets.
  */
-const fetchTweets = (screenName, count = 200) => {
-  return new Promise((resolve, reject) => {
+const fetchTweets = (screenName, count = 200) => (
+  new Promise((resolve, reject) => {
     client.get('statuses/user_timeline', {
       screen_name: screenName,
-      count
-    }, (error, tweets, response) => {
+      count,
+    }, (error, tweets) => {
       if (error) {
         return reject(error);
       }
-      resolve(tweets);
+      return resolve(tweets);
     });
-  });
-};
+  })
+);
 
 /**
  * Fetches past 200 tweets from specified username and uses Markov to generate a new sentance.
  * @param {string} screenName Name of user for which a tweet will be generated.
  * @return {object} Promise object that resolves a markov-generated sentance.
  */
-const generateTweet = (screenName) => {
-  return new Promise((resolve, reject) => {
+const generateTweet = (screenName) => (
+  new Promise((resolve, reject) => {
     // Fetch last 200 tweets from screenName.
     fetchTweets(screenName).then((tweets) => {
       // Create a markov chain generator.
       const markov = new Markov(tweets.map(tweet => tweet.text), {
         stateSize: 1,
         maxLength: 123,
-        minScore: 10
+        minScore: 10,
       });
 
       // Build corpus, generate sent
@@ -50,25 +50,24 @@ const generateTweet = (screenName) => {
         resolve(result.string);
       }).catch(reject);
     }).catch(reject);
-  });
-};
+  })
+);
 
 /**
  * Generates a tweet and sends it to realDonaldTrump.
- * @param {object} event AWS Lambda event that invoked this handler.
- * @param {object} context Context given from AWS Lambda.
+ * @returns {undefined} nothing.
  */
-const tweetToTrump = (event, context) => {
+const tweetToTrump = () => {
   const screenName = 'realDonaldTrump';
   generateTweet(screenName).then((sentance) => {
     client.post('statuses/update', { status: `@${screenName} ${sentance}` },
-      (error, tweet, response) => {
+      (error) => {
         if (error) {
           console.log(error);
         }
       }
     );
   }).catch(console.log);
-}
+};
 
 exports.handler = tweetToTrump;
